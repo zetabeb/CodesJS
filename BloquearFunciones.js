fdefb87c78476d23cd27ac6ee7f0640f9ebc4d56
Zamir <59321983+zetabeb@users.noreply.github.com>
2021-10-17 00:00:18 -0500

    agregados comentarios

--- BloquearFunciones.js
+++ BloquearFunciones.js
@@ -1,9 +1,10 @@
-
-    $(document).bind("contextmenu", function (e) {
-        e.preventDefault();
-    });
-    $(document).keydown(function (e) {
-        if (e.which === 123) {
-            return false;
-        }
-    });
+//para bloquear el clic derecho
+$(document).bind("contextmenu", function (e) {
+    e.preventDefault();
+});
+//para bloquear la tecla f12
+$(document).keydown(function (e) {
+    if (e.which === 123) {
+        return false;
+    }
+});
